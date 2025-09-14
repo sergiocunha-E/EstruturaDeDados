81d678f0ace75485299fe724ec72aa50c17f8c89
Sergio cunha <sergiocunha.m.s@gmail.com>
2025-09-14 15:24:45 -0300

    Questão 01

--- ListaJava/Questão01.java
+++ ListaJava/Questão01.java
@@ -1,5 +1,26 @@
 package ListaJava;
 
+import java.util.Scanner; // import vai aqui
+
 public class Questão01 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] numeros = new int[5]; // vetor de 5 posições
+        
+        // leitura dos números
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Digite o número " + (i + 1) + ": ");
+            numeros[i] = sc.nextInt();
+        }
+        
+        System.out.println("\nNúmeros digitados:");
+        
+        // exibição dos números
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Posição " + i + ": " + numeros[i]);
+        }
+        
+        sc.close();
+    }
 }
