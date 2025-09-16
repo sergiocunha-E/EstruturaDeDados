ee6176eb5f02b275a052a7bb84cfc46d9bf2be8b
Sergio cunha <sergiocunha.m.s@gmail.com>
2025-09-16 12:22:06 -0300

    Questão 01

--- ListaJava/Questao01.java
+++ ListaJava/Questao01.java
@@ -6,7 +6,7 @@ public class Questao01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int[] numeros = new int[5]; 
+        int[] numeros = new int[5];  //
         
         
         for (int i = 0; i < numeros.length; i++) {
