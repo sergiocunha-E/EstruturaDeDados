87f932ec1555bff13fe40ea282e09e15500cf472
Sergio cunha <sergiocunha.m.s@gmail.com>
2025-09-25 00:17:16 -0300

    aula dia 22 de setembro

--- heap/src/ArvoreBinariaHeapMaxima.java
+++ heap/src/ArvoreBinariaHeapMaxima.java
@@ -1,12 +1,9 @@
-package heap.src;
-
 public class ArvoreBinariaHeapMaxima {
     private Object[] data;
     int tail;
 
-    public ArvoreBinariaHeapMaxima(Object[] data) {
-        this.data = data;
-        this.tail = -1;
+    public ArvoreBinariaHeapMaxima() {
+       this(15);
     }
 
     public ArvoreBinariaHeapMaxima(int length) {
@@ -14,9 +11,61 @@ public class ArvoreBinariaHeapMaxima {
         tail=-1;
     }
 
-    public void inserir(Object data){
-        if (isFull()) {
-            System.err.println("Heap Cheia!");
+    public int parent(int son){
+       return  (son-1)/2;
+    }
+    public int leftSon(int son){
+        return (son*2)+1;
+    }
+    public int rightSon(int son){
+        return (son*2)+2;
+    }
+
+    public void enqueue(Object data){
+        Object temp=null;
+        if(isFull()){
+            System.err.println("Heap is full!");
         }
+        else{
+            tail++;
+            this.data[tail]=data;
+            heapifyUp(tail);
+        }
+
+    }
+
+
+
+    private void swap(int index1,int index2){
+        Object temp=data[index1];
+        data[index1]=data[index2];
+        data[index2]=temp;
+    }
+
+    private void  heapifyUp(int index){
+        int temp=parent(index);
+        while((Integer)data[index]>(Integer)data[temp]){
+            swap(index,temp);
+            index=temp;
+            temp=parent(index);
+        }
+    }
+
+    private void  heapifyDown(int index){
+        int temp=parent(index);
+        while((Integer)data[index]>(Integer)data[temp]){
+            swap(index,temp);
+            index=temp;
+            temp=parent(index);
+        }
+    }
+
+
+    public boolean isEmpty() {
+        return tail == -1;
+    }
+
+    public boolean isFull() {
+        return tail == data.length - 1;
     }
 }
