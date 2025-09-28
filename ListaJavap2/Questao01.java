bcf6ee7d59d4d4de52c5455be785f00089f4b443
Sergio cunha <sergiocunha.m.s@gmail.com>
2025-09-27 21:40:52 -0300

    Questão 01

---
+++ ListaJavap2/Questao01.java
@@ -0,0 +1,15 @@
+package ListaJavap2;
+
+public class Questao01 {
+   
+    public static void main(String[] args) {
+        
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        System.out.println("Elementos do array:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Posição " + i + " = " + numeros[i]);
+        }
+    }
+}
+
